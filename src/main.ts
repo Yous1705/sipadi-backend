71f73407d9191e7c722061c5fc952f02629b084e
Yous Sibarani <youssibarani17@gmail.com>
2026-01-19 19:58:37 +0700

    fix cors origin

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['https://<domain-frontend-kamu>.up.railway.app'],
+    origin: ['sipadi-fe-production.up.railway.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
