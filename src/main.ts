969bace8028801a2b61f04b77c232283c5a0ba23
Yous Sibarani <youssibarani17@gmail.com>
2026-05-31 23:32:24 +0700

    fix(cors) add FE url to allowlist

--- src/main.ts
+++ src/main.ts
@@ -33,38 +33,38 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  // app.enableCors({
+  //   origin: ['http://localhost:3001'],
+  //   credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
+
+  // await app.listen(process.env.PORT ?? 3000);
+
+  const allowlist = new Set([
+    'https://sipadi-fe-production.up.railway.app',
+    'http://localhost:3000',
+    'http://localhost:3001',
+  ]);
+
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: (origin, cb) => {
+      if (!origin) return cb(null, true);
+
+      if (allowlist.has(origin)) return cb(null, true);
+
+      return cb(new Error(`CORS blocked for origin: ${origin}`), false);
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 
-  //   const allowlist = new Set([
-  //     'https://sipadi-fe-production.up.railway.app',
-  //     'http://localhost:3000',
-  //     'http://localhost:3001',
-  //   ]);
-
-  //   app.enableCors({
-  //     origin: (origin, cb) => {
-  //       if (!origin) return cb(null, true);
-
-  //       if (allowlist.has(origin)) return cb(null, true);
-
-  //       return cb(new Error(`CORS blocked for origin: ${origin}`), false);
-  //     },
-  //     credentials: true,
-  //     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  //     allowedHeaders: ['Content-Type', 'Authorization'],
-  //   });
-
-  //   const port = Number(process.env.PORT) || 3000;
-  //   await app.listen(port, '0.0.0.0');
-
-  //   console.log(`API running on port ${port}`);
+  console.log(`API running on port ${port}`);
   // }
 }
 
